7e1f477e51b80322e14903812ba1ae51ab2b86e1
bryan albrecht <bryan.albrecht@noser.com>
2016-11-11 15:30:54 +0100

    Added visual comments

--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -12,9 +12,13 @@ namespace ChainOfResponsibility
             IChain chainelement2 = new Subtract() { NextChain = chainelement3 };
             IChain chainelement1 = new Add() { NextChain = chainelement2 };
 
+            Console.WriteLine("Addition:");
             chainelement1.Calculate(new CalculateObject(10, 5, CalculationType.add));
+            Console.WriteLine("Division:");
             chainelement1.Calculate(new CalculateObject(10, 5, CalculationType.div));
+            Console.WriteLine("Muliplikation");
             chainelement1.Calculate(new CalculateObject(10, 5, CalculationType.mult));
+            Console.WriteLine("Substraktion");
             chainelement1.Calculate(new CalculateObject(10, 5, CalculationType.sub));
 
             Console.ReadKey();
